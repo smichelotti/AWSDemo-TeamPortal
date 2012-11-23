08b62aa8f056dbac90a972371b2e540eb6098ce1
Steve Michelotti <steve.michelotti@gmail.com>
2012-11-23 15:26:47 -0500

    interim commit

--- TeamPortal/Models/TeamPortalWebContext.cs
+++ TeamPortal/Models/TeamPortalWebContext.cs
@@ -23,14 +23,6 @@ namespace TeamPortal.Models
         public TeamPortalWebContext()
             : base("TeamPortalDB")
         {
-            var adapter = this as IObjectContextAdapter;
-            //adapter.ObjectContext.Connection.ConnectionString
-        }
-
-        public string WriteConnection()
-        {
-            var adapter = this as IObjectContextAdapter;
-            return adapter.ObjectContext.Connection.ConnectionString;
         }
 
         public DbSet<TeamPortal.Models.Player> Players { get; set; }
